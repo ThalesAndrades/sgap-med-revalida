9be35a2e1e28bfc32ff444ca73dbe5e85eeff363
ThalesAndrades <thales@ailun.com.br>
2026-02-26 23:27:56 -0300

    chore: remove debug test php endpoints

--- public/api/minlib_test.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-header('Content-Type: application/json; charset=utf-8');
-
-require_once __DIR__ . '/minlib.php';
-
-echo json_encode(['ok' => minlib_ok()], JSON_UNESCAPED_UNICODE);
-
--- public/api/ping.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-header('Content-Type: application/json; charset=utf-8');
-echo json_encode(['ok' => true], JSON_UNESCAPED_UNICODE);
-
